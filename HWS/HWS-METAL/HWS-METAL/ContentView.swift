4f7bb02177208cceb0f038705510454e440384ee
Furkan Alioglu <91939823+furkanalioglu@users.noreply.github.com>
2024-05-28 08:21:53 +0300

    feat: hws metal anims

--- HWS/HWS-METAL/HWS-METAL/ContentView.swift
+++ HWS/HWS-METAL/HWS-METAL/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            HeaderView(title: "Rainbow Effect")
+            AnimationExampleView(effectType: .rainbow)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.blue.opacity(0.1))
+                .cornerRadius(10)
+                .shadow(radius: 5)
+            
+            Divider()
+                .padding(.vertical, 5)
+            
+            HeaderView(title: "Wave Effect")
+            AnimationExampleView(effectType: .wave)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.green.opacity(0.1))
+                .cornerRadius(10)
+                .shadow(radius: 5)
         }
         .padding()
+        .background(Color(.systemGray6))
+        .cornerRadius(20)
+        .shadow(radius: 10)
+        .padding()
+    }
+}
+
+struct HeaderView: View {
+    let title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.headline)
+            .padding(.bottom, 5)
     }
 }
 
